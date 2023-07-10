e0560cfca10adac42053cee8a64221e3ca3b1e2d
Sam Esserman <sesserman97@gmail.com>
2023-07-10 09:07:03 -0400

    completed post file

---
+++ blogify/src/components/Post.jsx
@@ -0,0 +1,10 @@
+
+const Post = ({ post }) => {
+  return (
+    <div className="post">
+      <h2>{post.title}</h2>
+      <p>{post.content}</p>
+      {/* Add like and comment functionality here */}
+    </div>
+  )
+}
